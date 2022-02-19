52b2c85e85e3561b7a518c0c780ffc3901f2e704
cyric2020 <60243062+cyric2020@users.noreply.github.com>
2022-02-19 19:56:54 +1000

    damn type

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.get('/imagelist', (req, res) => {
   var images = [];
 
   fs.readdir('./images/', (err, files) => {
-    if(files == undefined || filex == null) return [];
+    if(files == undefined || files == null) return [];
     files.forEach(file => {
       var filename = file.replace(/\.[^/.]+$/, "");
       var size = fs.statSync(`./images/${file}`).size;
